1341734024ec6be33f73dfe3002d19df55bfbf1d
Jakob Schikowski <jakob.schikowski@gmx.de>
2020-12-03 00:10:06 +0100

    Add `props::symmetric_binrel`

--- src/props/binrel.rs
+++ src/props/binrel.rs
@@ -1,6 +1,6 @@
 use std::fmt::Debug;
 
-use crate::{hint_section, ops, Fun2, Var1};
+use crate::{hint_section, ops, Fun2, Var1, Var2};
 
 /// Asserts that the binary relation `rel` is reflexive.
 ///
@@ -18,6 +18,22 @@ where
     ops::assert(rel.eval_once(a.clone(), a));
 }
 
+/// Asserts that the binary relation `rel` is symmetric.
+///
+/// For `a`, `b` of `set` it must hold:
+/// - `rel(a, b) --> rel(b, a)`
+pub fn symmetric_binrel<T, R>(set: Var2<T>, rel: Fun2<R>)
+where
+    T: Debug + Clone,
+    R: Fn(T, T) -> bool,
+{
+    hint_section!("Is `{}` symmetric?", rel.name);
+
+    let [a, b] = set.eval();
+
+    ops::assert(ops::implies(rel.eval(a.clone(), b.clone()), rel.eval(b, a)));
+}
+
 #[cfg(test)]
 mod tests {
     use crate::{infix_fun_2, props, FateVarExt};
@@ -25,10 +41,19 @@ mod tests {
 
     #[test]
     fn reflexive_binrel_example() {
-        Dicetest::repeatedly().run(|mut fate| {
+        Dicetest::once().run(|mut fate| {
             let rel = infix_fun_2("==", |x, y| x == y);
             let set = fate.roll_var_1("u8", "x", dice::u8(..));
             props::reflexive_binrel(set, rel);
+        })
+    }
+
+    #[test]
+    fn symmetric_binrel_example() {
+        Dicetest::once().run(|mut fate| {
+            let rel = infix_fun_2("!=", |x, y| x != y);
+            let set = fate.roll_var_2("f32", ["x", "y"], dice::f32(..));
+            props::symmetric_binrel(set, rel);
         })
     }
 }
