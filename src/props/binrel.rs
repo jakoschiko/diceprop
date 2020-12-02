67112a65f47b6d2e144f0f919919e9ff459a6dca
Jakob Schikowski <jakob.schikowski@gmx.de>
2020-12-03 00:57:36 +0100

    Rename local variables

--- src/props/binrel.rs
+++ src/props/binrel.rs
@@ -4,48 +4,48 @@ use crate::{hint_section, ops, var_1, var_2, var_3, Fun2, Var1, Var2, Var3};
 
 /// Asserts that the binary relation `rel` is reflexive.
 ///
-/// For `a` of `set` it must hold:
+/// For `a` of `var.set` it must hold:
 /// - `rel(a, a)`
-pub fn reflexive_binrel<T, R>(set: Var1<T>, rel: Fun2<R>)
+pub fn reflexive_binrel<S, R>(var: Var1<S>, rel: Fun2<R>)
 where
-    T: Debug + Clone,
-    R: FnOnce(T, T) -> bool,
+    S: Debug + Clone,
+    R: FnOnce(S, S) -> bool,
 {
     hint_section!("Is `{}` reflexive?", rel.name);
 
-    let a = set.eval();
+    let a = var.eval();
 
     ops::assert(rel.eval_once(a.clone(), a));
 }
 
 /// Asserts that the binary relation `rel` is symmetric.
 ///
-/// For `a`, `b` of `set` it must hold:
+/// For `a`, `b` of `var.set` it must hold:
 /// - `rel(a, b) --> rel(b, a)`
-pub fn symmetric_binrel<T, R>(set: Var2<T>, rel: Fun2<R>)
+pub fn symmetric_binrel<S, R>(var: Var2<S>, rel: Fun2<R>)
 where
-    T: Debug + Clone,
-    R: Fn(T, T) -> bool,
+    S: Debug + Clone,
+    R: Fn(S, S) -> bool,
 {
     hint_section!("Is `{}` symmetric?", rel.name);
 
-    let [a, b] = set.eval();
+    let [a, b] = var.eval();
 
     ops::assert(ops::implies(rel.eval(a.clone(), b.clone()), rel.eval(b, a)));
 }
 
 /// Asserts that the binary relation `rel` is antisymmetric.
 ///
-/// For `a`, `b` of `set` it must hold:
+/// For `a`, `b` of `var.set` it must hold:
 /// - `a != b && rel(a, b) --> !rel(b, a)`
-pub fn antisymmetric_binrel<T, R>(set: Var2<T>, rel: Fun2<R>)
+pub fn antisymmetric_binrel<S, R>(var: Var2<S>, rel: Fun2<R>)
 where
-    T: Debug + Clone + PartialEq,
-    R: Fn(T, T) -> bool,
+    S: Debug + Clone + PartialEq,
+    R: Fn(S, S) -> bool,
 {
     hint_section!("Is `{}` antisymmetric?", rel.name);
 
-    let [a, b] = set.eval();
+    let [a, b] = var.eval();
 
     ops::assert(ops::implies(
         ops::and(
@@ -58,16 +58,16 @@ where
 
 /// Asserts that the binary relation `rel` is transitive.
 ///
-/// For `a`, `b`, `c` of `set` it must hold:
+/// For `a`, `b`, `c` of `var.set` it must hold:
 /// - `rel(a, b) && rel(b, c) --> rel(a, c)`
-pub fn transitive_binrel<T, R>(set: Var3<T>, rel: Fun2<R>)
+pub fn transitive_binrel<S, R>(var: Var3<S>, rel: Fun2<R>)
 where
-    T: Debug + Clone,
-    R: Fn(T, T) -> bool,
+    S: Debug + Clone,
+    R: Fn(S, S) -> bool,
 {
     hint_section!("Is `{}` transitive?", rel.name);
 
-    let [a, b, c] = set.eval();
+    let [a, b, c] = var.eval();
 
     ops::assert(ops::implies(
         ops::and(
@@ -83,19 +83,19 @@ where
 /// It must hold:
 /// - `rel` is symmetric ([`symmetric_binrel`])
 /// - `rel` is transitive ([`transitive_binrel`])
-pub fn partial_eq_binrel<T, R>(set: Var3<T>, rel: Fun2<R>)
+pub fn partial_eq_binrel<S, R>(var: Var3<S>, rel: Fun2<R>)
 where
-    T: Debug + Clone,
-    R: Fn(T, T) -> bool,
+    S: Debug + Clone,
+    R: Fn(S, S) -> bool,
 {
     hint_section!("Is `{}` a partial equality relation?", rel.name);
 
-    let [a, b, c] = set.elems;
-    let set_2 = var_2(set.set, [a.clone(), b.clone()]);
-    let set_3 = var_3(set.set, [a, b, c]);
+    let [a, b, c] = var.elems;
+    let var_2 = var_2(var.set, [a.clone(), b.clone()]);
+    let var_3 = var_3(var.set, [a, b, c]);
 
-    symmetric_binrel(set_2, rel.as_ref());
-    transitive_binrel(set_3, rel.as_ref());
+    symmetric_binrel(var_2, rel.as_ref());
+    transitive_binrel(var_3, rel.as_ref());
 }
 
 /// Asserts that the binary relation `rel` is an equality relation.
@@ -104,36 +104,36 @@ where
 /// - `rel` is reflexive ([`reflexive_binrel`])
 /// - `rel` is symmetric ([`symmetric_binrel`])
 /// - `rel` is transitive ([`transitive_binrel`])
-pub fn eq_binrel<T, R>(set: Var3<T>, rel: Fun2<R>)
+pub fn eq_binrel<S, R>(var: Var3<S>, rel: Fun2<R>)
 where
-    T: Debug + Clone,
-    R: Fn(T, T) -> bool,
+    S: Debug + Clone,
+    R: Fn(S, S) -> bool,
 {
     hint_section!("Is `{}` an equality relation?", rel.name);
 
-    let [a, b, c] = set.elems;
-    let set_1 = var_1(set.set, a.clone());
-    let set_2 = var_2(set.set, [a.clone(), b.clone()]);
-    let set_3 = var_3(set.set, [a, b, c]);
+    let [a, b, c] = var.elems;
+    let var_1 = var_1(var.set, a.clone());
+    let var_2 = var_2(var.set, [a.clone(), b.clone()]);
+    let var_3 = var_3(var.set, [a, b, c]);
 
-    reflexive_binrel(set_1, rel.as_ref());
-    symmetric_binrel(set_2, rel.as_ref());
-    transitive_binrel(set_3, rel.as_ref());
+    reflexive_binrel(var_1, rel.as_ref());
+    symmetric_binrel(var_2, rel.as_ref());
+    transitive_binrel(var_3, rel.as_ref());
 }
 
 /// Asserts that the binary relation `erel` is equivalent to the relation `rel`.
 ///
-/// For all `a`, `b` of `set` it must hold:
+/// For all `a`, `b` of `var.set` it must hold:
 /// - `rel(a, b) <-> erel(a, b)`
-pub fn equivalent_binrel<T, R, E>(set: Var2<T>, rel: Fun2<R>, erel: Fun2<E>)
+pub fn equivalent_binrel<S, R, E>(var: Var2<S>, rel: Fun2<R>, erel: Fun2<E>)
 where
-    T: Debug + Clone,
-    R: FnOnce(T, T) -> bool,
-    E: FnOnce(T, T) -> bool,
+    S: Debug + Clone,
+    R: FnOnce(S, S) -> bool,
+    E: FnOnce(S, S) -> bool,
 {
     hint_section!("Is `{}` equivalent to `{}`?", erel.name, rel.name);
 
-    let [a, b] = set.eval();
+    let [a, b] = var.eval();
 
     ops::assert(ops::iff(
         rel.eval_once(a.clone(), b.clone()),
@@ -143,13 +143,13 @@ where
 
 /// Asserts that the binary relation `crel` is the complementary relation of `rel`.
 ///
-/// For all `a`, `b` of `set` it must hold:
+/// For all `a`, `b` of `var.set` it must hold:
 /// - `rel(a, b) <-> !crel(a, b)`
-pub fn complementary_binrel<T, R, C>(set: Var2<T>, rel: Fun2<R>, crel: Fun2<C>)
+pub fn complementary_binrel<S, R, C>(var: Var2<S>, rel: Fun2<R>, crel: Fun2<C>)
 where
-    T: Debug + Clone,
-    R: FnOnce(T, T) -> bool,
-    C: FnOnce(T, T) -> bool,
+    S: Debug + Clone,
+    R: FnOnce(S, S) -> bool,
+    C: FnOnce(S, S) -> bool,
 {
     hint_section!(
         "Is `{}` complementary relation of `{}`?",
@@ -157,7 +157,7 @@ where
         rel.name,
     );
 
-    let [a, b] = set.eval();
+    let [a, b] = var.eval();
 
     ops::assert(ops::iff(
         rel.eval_once(a.clone(), b.clone()),
@@ -167,15 +167,16 @@ where
 
 #[cfg(test)]
 mod tests {
-    use crate::{infix_fun_2, props, FateVarExt};
     use dicetest::prelude::*;
+
+    use crate::{infix_fun_2, props, FateVarExt};
 
     #[test]
     fn reflexive_binrel_example() {
         Dicetest::once().run(|mut fate| {
             let rel = infix_fun_2("==", |x, y| x == y);
-            let set = fate.roll_var_1("u8", "x", dice::u8(..));
-            props::reflexive_binrel(set, rel);
+            let var = fate.roll_var_1("u8", "x", dice::u8(..));
+            props::reflexive_binrel(var, rel);
         })
     }
 
@@ -183,8 +184,8 @@ mod tests {
     fn symmetric_binrel_example() {
         Dicetest::once().run(|mut fate| {
             let rel = infix_fun_2("!=", |x, y| x != y);
-            let set = fate.roll_var_2("f32", ["x", "y"], dice::any_f32());
-            props::symmetric_binrel(set, rel);
+            let var = fate.roll_var_2("f32", ["x", "y"], dice::any_f32());
+            props::symmetric_binrel(var, rel);
         })
     }
 
@@ -192,8 +193,8 @@ mod tests {
     fn antisymmetric_binrel_example() {
         Dicetest::once().run(|mut fate| {
             let rel = infix_fun_2("<", |x, y| x < y);
-            let set = fate.roll_var_2("u8", ["x", "y"], dice::u8(..));
-            props::antisymmetric_binrel(set, rel);
+            let var = fate.roll_var_2("u8", ["x", "y"], dice::u8(..));
+            props::antisymmetric_binrel(var, rel);
         })
     }
 
@@ -201,8 +202,8 @@ mod tests {
     fn transitive_binrel_example() {
         Dicetest::once().run(|mut fate| {
             let rel = infix_fun_2("<", |x, y| x < y);
-            let set = fate.roll_var_3("char", ["x", "y", "z"], dice::char());
-            props::transitive_binrel(set, rel);
+            let var = fate.roll_var_3("char", ["x", "y", "z"], dice::char());
+            props::transitive_binrel(var, rel);
         })
     }
 
@@ -210,8 +211,8 @@ mod tests {
     fn partial_eq_binrel_example() {
         Dicetest::once().run(|mut fate| {
             let rel = infix_fun_2("==", |x, y| x == y);
-            let set = fate.roll_var_3("f32", ["x", "y", "z"], dice::any_f32());
-            props::partial_eq_binrel(set, rel);
+            let var = fate.roll_var_3("f32", ["x", "y", "z"], dice::any_f32());
+            props::partial_eq_binrel(var, rel);
         })
     }
 
@@ -219,8 +220,8 @@ mod tests {
     fn eq_binrel_example() {
         Dicetest::once().run(|mut fate| {
             let rel = infix_fun_2("==", |x, y| x == y);
-            let set = fate.roll_var_3("String", ["x", "y", "z"], dice::string(dice::char(), ..));
-            props::eq_binrel(set, rel);
+            let var = fate.roll_var_3("String", ["x", "y", "z"], dice::string(dice::char(), ..));
+            props::eq_binrel(var, rel);
         })
     }
 
@@ -229,8 +230,8 @@ mod tests {
         Dicetest::once().run(|mut fate| {
             let rel = infix_fun_2("==", |x, y| x == y);
             let erel = infix_fun_2("!!=", |x, y| !(x != y));
-            let set = fate.roll_var_2("u8", ["x", "y"], dice::u8(..));
-            props::equivalent_binrel(set, rel, erel);
+            let var = fate.roll_var_2("u8", ["x", "y"], dice::u8(..));
+            props::equivalent_binrel(var, rel, erel);
         })
     }
 
@@ -239,8 +240,8 @@ mod tests {
         Dicetest::once().run(|mut fate| {
             let rel = infix_fun_2("<", |x, y| x < y);
             let crel = infix_fun_2(">=", |x, y| x >= y);
-            let set = fate.roll_var_2("u8", ["x", "y"], dice::u8(..));
-            props::complementary_binrel(set, rel, crel);
+            let var = fate.roll_var_2("u8", ["x", "y"], dice::u8(..));
+            props::complementary_binrel(var, rel, crel);
         })
     }
 }
