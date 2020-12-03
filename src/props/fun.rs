f75e7c5da63816f194244a8bdb3f74da1203a909
Jakob Schikowski <jakob.schikowski@gmx.de>
2020-12-03 18:02:54 +0100

    Add `props::idempotent_fun`

--- src/props/fun.rs
+++ src/props/fun.rs
@@ -1,6 +1,23 @@
 use std::fmt::Debug;
 
-use crate::{hint_section, ops, Fun2, Var2};
+use crate::{hint_section, ops, Fun1, Fun2, Var1, Var2};
+
+/// Asserts that the function `f` is idempotent.
+///
+/// For `a` of `var.set` it must hold:
+/// - `f(a) == f(f(a))`
+pub fn idempotent_fun<S, F>(var: Var1<S>, f: Fun1<F>)
+where
+    S: Debug + Clone + PartialEq,
+    F: Fn(S) -> S,
+{
+    hint_section!("Is `{}` idempotent?", f.name);
+
+    let a = var.eval();
+    let fa = f.eval(a);
+
+    ops::assert(ops::eq(fa.as_ref(), f.eval(fa.clone()).as_ref()));
+}
 
 /// Asserts that the function `f` is commutative.
 ///
@@ -24,10 +41,26 @@ where
 
 #[cfg(test)]
 mod tests {
-    use crate::{fun_2, props, FateVarExt};
+    use crate::{fun_1, fun_2, props, FateVarExt};
 
     use dicetest::prelude::*;
     use std::collections::BTreeSet;
+
+    #[test]
+    fn idempotent_fun_example() {
+        Dicetest::once().run(|mut fate| {
+            let var = fate.roll_var_1(
+                "String",
+                "x",
+                dice::string(
+                    dice::one_of_die_2(dice::one_of_slice(&[' ', '\n', '\t']), dice::char()),
+                    ..,
+                ),
+            );
+            let f = fun_1("trim", |x: String| x.trim().to_owned());
+            props::idempotent_fun(var, f);
+        })
+    }
 
     #[test]
     fn commutative_fun_example() {
