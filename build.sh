74e698223c966f9c1a4b0b85573f8586b82b7130
Jakob Schikowski <jakob.schikowski@gmx.de>
2021-03-20 13:40:14 +0100

    Add build script

---
+++ build.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+set -e
+
+echo "STEP diceprop: cargo fmt"
+cargo fmt
+
+echo "STEP diceprop: cargo build"
+cargo build
+
+echo "STEP diceprop: cargo test -- --format=terse"
+cargo test -- --format=terse
+
+echo "STEP diceprop: cargo clippy"
+cargo clippy
+
+echo "STEP diceprop: cargo doc --no-deps"
+cargo doc --no-deps
+
+echo "STEP diceprop: cargo readme > README.md"
+cargo readme > README.md
+
+
+cd examples_readme
+
+echo "STEP examples_readme: cargo fmt"
+cargo fmt
+
+echo "STEP examples_readme: cargo build"
+cargo build
+
+echo "STEP examples_readme: cargo clippy"
+cargo clippy
